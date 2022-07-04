a0a05577621c9740f15bc1578cd7f32d01005324
Oskar Malm Wiklund <oskar.malm.wiklund@gmail.com>
2022-07-04 16:17:08 +0200

    Generate random number between 1-3 in function computerPlay

--- javascript.js
+++ javascript.js
@@ -2,11 +2,22 @@
 function computerPlay(){
 // Randomly generate a number between 1-3 and store in variable randomNumber
 let randomNumber = Math.floor(Math.random()*3+1);
-console.log(randomNumber);
+
+if (randomNumber === 1){
+    // If randomNumber = 1, console.log("Rock")
+    console.log("Rock");
+    } else if(randomNumber === 2){
+        // If randomNumber = 2, console.log("Paper")
+        console.log("Paper")
+    } else {
+    // If randomNumber = 1, console.log("Scissors")
+        console.log("Scissors")
+    }
 }
 
 computerPlay();
 
-// If number 1 is generated console.log("Rock")
-// If number 2 is generated console.log("Paper")
-// If number 1 is generated console.log("Scissors")
+// Create function playRound with two parameters, playerSelection and computerSelection
+function playRound(playerSelection, computerSelection){
+
+}
