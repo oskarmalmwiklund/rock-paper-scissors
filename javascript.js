94b5a20e7192d0639d8d102bf1229cc313a66890
Oskar Malm Wiklund <oskar.malm.wiklund@gmail.com>
2022-07-04 16:29:48 +0200

    Add function playRound

--- javascript.js
+++ javascript.js
@@ -4,11 +4,11 @@ function computerPlay(){
 let randomNumber = Math.floor(Math.random()*3+1);
 
 if (randomNumber === 1){
-    // If randomNumber = 1, return "Rock"
-     return "Rock";
+        // If randomNumber = 1, return "Rock"
+        return "Rock";
     } else if(randomNumber === 2){
         // If randomNumber = 2, return "Paper"
-      return "Paper";
+        return "Paper";
     } else {
         // If randomNumber = 1, return "Scissors"
         return "Scissors";
@@ -17,5 +17,9 @@ if (randomNumber === 1){
 
 // Create function playRound with two parameters, playerSelection and computerSelection
 function playRound(playerSelection, computerSelection){
-
+    return computerSelection + " " + playerSelection;
 }
+
+const playerSelection = "rock";
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
