4dbd8578d7097181e85d65e30b08eeb04e8c8bc3
Oskar Malm Wiklund <oskar.malm.wiklund@gmail.com>
2022-07-04 16:07:27 +0200

    Add computerPlay function

--- javascript.js
+++ javascript.js
@@ -1,1 +1,12 @@
-console.log("This is a test");
+// Create a function called computerPlay
+function computerPlay(){
+// Randomly generate a number between 1-3 and store in variable randomNumber
+let randomNumber = Math.floor(Math.random()*3+1);
+console.log(randomNumber);
+}
+
+computerPlay();
+
+// If number 1 is generated console.log("Rock")
+// If number 2 is generated console.log("Paper")
+// If number 1 is generated console.log("Scissors")
